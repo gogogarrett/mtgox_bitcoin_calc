969d607b70e1101ce8fd68daeebb8c57da2b5c52
Garrett Heinlen <heinleng@gmail.com>
2012-08-01 18:02:38 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MtgoxBitcoinCalc::Application.config.secret_token = 'e2e3f49878adf2d8a7380f4dd4819a596f8513d3fdb5ea75692b9f5d60ce7b9743e49121e490eeb8b4bfb2887af065125d5228f157145ff3d5940e87c25796a8'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+MtgoxBitcoinCalc::Application.routes.draw do
+  root :to => "home#index"
+end
